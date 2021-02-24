8b041a9b00de9e2eeff1eb2dc57fe0c769f7bd4f
yxl <18217692140@163.COM>
2021-02-24 17:21:38 +0800

    解释器模式

---
+++ src/main/java/com/yxl/design/Interpreter/SubExpression.java
@@ -0,0 +1,21 @@
+package com.yxl.design.Interpreter;
+
+import java.util.HashMap;
+
+/**
+ * @author yxl
+ * @version 1.0
+ * @date 2021/2/24 16:21
+ */
+public class SubExpression extends SymbolExpression{
+
+    public SubExpression(Expression left, Expression right) {
+        super(left, right);
+    }
+
+
+    @Override
+    public int interpreter(HashMap<String, Integer> var) {
+        return super.left.interpreter(var) - super.right.interpreter(var);
+    }
+}
---
+++ src/main/java/com/yxl/design/Interpreter/VarExpression.java
@@ -0,0 +1,22 @@
+package com.yxl.design.Interpreter;
+
+import java.util.HashMap;
+
+/**
+ * @author yxl
+ * @version 1.0
+ * @date 2021/2/24 16:16
+ */
+public class VarExpression extends Expression{
+
+    private String key;
+
+    public VarExpression(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public int interpreter(HashMap<String, Integer> var) {
+        return var.get(key);
+    }
+}
